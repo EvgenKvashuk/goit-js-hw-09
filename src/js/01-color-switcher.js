38e56823eea64af4bfbca3623ab9a7872f66f835
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-04-13 22:36:11 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,29 @@
 let timerId = null;
-DELAY_VALUE = 1000;
+const DELAY_VALUE = 1000;
 
+// посилання на елементи
 const refs = {
   body: document.querySelector("body"),
   startBtn: document.querySelector(".js-start"),
   stopBtn: document.querySelector(".js-stop"),
 };
 
+// кнопка запуску
 refs.startBtn.addEventListener("click", () => {
   timerId = setInterval(() => {
     function getRandomHexColor() {
       return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
     }
-    console.log(getRandomHexColor())
+    // console.log(getRandomHexColor())
     refs.body.style.backgroundColor = getRandomHexColor();
   }, DELAY_VALUE);
+  refs.startBtn.setAttribute("disabled", "disabled");
 });
 
+// кнопка зупинки
 refs.stopBtn.addEventListener("click", () => {
   clearInterval(timerId);
-  console.log(`Interval with id ${timerId} has stopped!`);
+  // console.log(`Interval with id ${timerId} has stopped!`);
+  var b = document.querySelector(".js-start");
+  refs.startBtn.removeAttribute("disabled", "disabled");
 });
-
-
-// при запуску одного інтервалу цого можна зупинити, 
-// але якщо запустити два і більше то їх вже не можливо зупинити. 
-// Як можна обмежити запуск більше двух інтервалів? Бо чим більше інтервалів тим частіше міняється фон.
