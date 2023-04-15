ee89ebfb7afa49c239f161d08f5db6b809165d80
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-04-16 00:59:40 +0300

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,6 +12,8 @@ const refs = {
     days: document.querySelector('.days-js'),
 };
 
+refs.startBtn.setAttribute("disabled", "disabled");
+
 const options = {
     "locale": Ukrainian,
     enableTime: true,
@@ -19,38 +21,11 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        // console.log(selectedDates[0]);
     },
 };
 
-const deadLine = new Date (2024, 0, 1)
-const CURRENT_DATA = new Date();
-const delta = deadLine - CURRENT_DATA;
-console.log(delta)
-const toSeconds = delta / 1000;
-
 const fp = flatpickr("input#datetime-picker", options);
 
-let choseDate = fp.selectedDates[0];
-
-let timerId = null;
-
-timerId = setInterval(() => {
-    const seconds = Math.floor(toSeconds) % 60;
-    const minutes = Math.floor(toSeconds / 60) % 60;
-    const hours = Math.floor(toSeconds / 60 / 60) % 24;
-    const days = Math.floor(toSeconds / 60 / 60 / 24);
-    
-    refs.seconds.textContent = seconds;
-    refs.minutes.textContent = minutes;
-    refs.hours.textContent = hours;
-    refs.days.textContent = days;
-
-    console.log(seconds)
-    console.log(minutes)
-    console.log(hours)
-    console.log(days)
-  }, 1000);
 
 
 
@@ -58,34 +33,24 @@ timerId = setInterval(() => {
 
 
 
+refs.inputDate.addEventListener("input", (evt) => {
+    let CURRENT_DATA = new Date();
 
 
-
-
-
-
-
-
-
-const onImput = refs.inputDate.addEventListener("onChange", (evt) => {
     let choseDate = fp.selectedDates[0];
-    refs.startBtn.setAttribute("disabled", "disabled")
-console.log("ввод")
+    // Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future".
+    if (choseDate > CURRENT_DATA) {
+        refs.startBtn.removeAttribute("disabled", "disabled");
+    }
+    // Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
+    // Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
+    if (choseDate < CURRENT_DATA) {
+        window.alert("Please choose a date in the future")
+    }
 });
 
 
 
-// const onStartCount = refs.startBtn.addEventListener("click", (evt) => {
-
-//     resultSelectedDat = fp.selectedDates[0];
-
-//     // console.log("перевірка");
-
-//     if (resultSelectedDat < CURRENT_DATA) {
-//         window.alert("Please choose a date in the future")
-//         // refs.startBtn.setAttribute("disabled", "disabled");
-//     } 
-// });
 
 
 
@@ -101,30 +66,77 @@ console.log("ввод")
 
 
 
-// Метод onClose() з об'єкта параметрів викликається 
-// щоразу під час закриття елемента інтерфейсу, 
-// який створює flatpickr. Саме у ньому варто обробляти дату,
-// обрану користувачем. Параметр selectedDates - це масив обраних дат,
-// тому ми беремо перший елемент.
 
-    // console.log(selectedDates[0]);
-    // 1 
-    // Якщо користувач вибрав дату в минулому, покажи window.alert() 
-    // з текстом "Please choose a date in the future".
-    // if (inputData <= CURRENT_DATA) {
 
-    //     window.alert("Please choose a date in the future")
-    // };
-    // 2
-    // Якщо користувач вибрав валідну дату (в майбутньому),
-    // кнопка «Start» стає активною.
 
-    // 3
-    // Кнопка «Start» повинна бути неактивною доти,
-    // доки користувач не вибрав дату в майбутньому.
+refs.inputDate.addEventListener("submit", (evt) => {
+    function convertMs(ms) {
+        // Number of milliseconds per unit of time
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
 
-    // 4
-    // Кнопка «Start» повинна бути неактивною доти,
-    // доки користувач не вибрав дату в майбутньому.
+        // Remaining days
+        const days = Math.floor(ms / day);
+        // Remaining hours
+        const hours = Math.floor((ms % day) / hour);
+        // Remaining minutes
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        // Remaining seconds
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
+        return { days, hours, minutes, seconds };
+    }
+
+    console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+    console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+    console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// запус таймера
+// let timerId = null;
+
+// timerId = setInterval(() => {
+//     let deadLine = new Date (2024, 0, 1)
+//     let CURRENT_DATA = new Date();
+//     let delta = deadLine - CURRENT_DATA;
+//     let toSeconds = delta / 1000;
+
+//     let seconds = Math.floor(delta / 1000) % 60;
+//     let minutes = Math.floor(delta / 1000 / 60) % 60;
+//     let hours = Math.floor(delta / 1000 / 60 / 60) % 24;
+//     let days = Math.floor(delta / 1000 / 60 / 60 / 24);
+
+//     refs.seconds.textContent = seconds;
+//     refs.minutes.textContent = minutes;
+//     refs.hours.textContent = hours;
+//     refs.days.textContent = days;
+
+//     // console.log(seconds)
+//     // console.log(minutes)
+//     // console.log(hours)
+//     // console.log(days)
+// }, 1000);
 
