a269282b3631adfd35dac5a0098160f71c626d61
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-04-16 11:31:14 +0300

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import { Ukrainian } from "flatpickr/dist/l10n/uk.js"
+import Notiflix from 'notiflix';
 
 const refs = {
     inputDate: document.querySelector('#datetime-picker'),
@@ -26,117 +27,45 @@ const options = {
 
 const fp = flatpickr("input#datetime-picker", options);
 
-
-
-
-
-
-
-
 refs.inputDate.addEventListener("input", (evt) => {
     let CURRENT_DATA = new Date();
-
-
     let choseDate = fp.selectedDates[0];
-    // Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future".
+    // Показує window.alert("Please choose a date in the future") якщо обрана дата в минулому.
     if (choseDate > CURRENT_DATA) {
         refs.startBtn.removeAttribute("disabled", "disabled");
     }
     // Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
-    // Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
+    // Кнопка «Start» неактивною доти, доки користувач не вибере дату в майбутньому.
     if (choseDate < CURRENT_DATA) {
-        window.alert("Please choose a date in the future")
+        // window.alert("Please choose a date in the future")
+        Notiflix.Notify.failure("Please choose a date in the future");
     }
 });
 
+refs.startBtn.addEventListener("click", (evt) => {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-refs.inputDate.addEventListener("submit", (evt) => {
-    function convertMs(ms) {
-        // Number of milliseconds per unit of time
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-
-        // Remaining days
-        const days = Math.floor(ms / day);
-        // Remaining hours
-        const hours = Math.floor((ms % day) / hour);
-        // Remaining minutes
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        // Remaining seconds
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-        return { days, hours, minutes, seconds };
+    function pad(value) {
+        return String(value).padStart(2, '0')
     }
 
-    console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-    console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-    console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+    let timerId = null;
+
+    timerId = setInterval(() => {
+        let choseDate = fp.selectedDates[0];
+        let CURRENT_DATA = new Date();
+        let delta = choseDate - CURRENT_DATA;
+        let toSeconds = delta / 1000;
+
+        let seconds = pad(Math.floor(delta / 1000) % 60);
+        let minutes = pad(Math.floor(delta / 1000 / 60) % 60);
+        let hours = pad(Math.floor(delta / 1000 / 60 / 60) % 24);
+        let days = Math.floor(delta / 1000 / 60 / 60 / 24);
+
+        refs.seconds.textContent = seconds;
+        refs.minutes.textContent = minutes;
+        refs.hours.textContent = hours;
+        refs.days.textContent = days;
+
+        // console.log(`${days}:${hours}:${minutes}:${seconds}`)
+    }, 1000);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// запус таймера
-// let timerId = null;
-
-// timerId = setInterval(() => {
-//     let deadLine = new Date (2024, 0, 1)
-//     let CURRENT_DATA = new Date();
-//     let delta = deadLine - CURRENT_DATA;
-//     let toSeconds = delta / 1000;
-
-//     let seconds = Math.floor(delta / 1000) % 60;
-//     let minutes = Math.floor(delta / 1000 / 60) % 60;
-//     let hours = Math.floor(delta / 1000 / 60 / 60) % 24;
-//     let days = Math.floor(delta / 1000 / 60 / 60 / 24);
-
-//     refs.seconds.textContent = seconds;
-//     refs.minutes.textContent = minutes;
-//     refs.hours.textContent = hours;
-//     refs.days.textContent = days;
-
-//     // console.log(seconds)
-//     // console.log(minutes)
-//     // console.log(hours)
-//     // console.log(days)
-// }, 1000);
-
