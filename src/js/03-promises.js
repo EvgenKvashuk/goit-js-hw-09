ab5d0b07f0d5c52c72fecf6d75dc5e4f001d28e2
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-04-20 21:24:27 +0300

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,52 +2,40 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   form: document.querySelector('.form'),
+
   delay: document.querySelector('.delay-js'),
   step: document.querySelector('.step-js'),
   amount: document.querySelector('.amount-js'),
-}
 
-refs.form.addEventListener('submit', onSubmitForm);
-
-function onSubmitForm(e) {
-  e.preventDefault();
-
-delay = Number(e.currentTarget.refs.delay.value);
-step = Number(e.currentTarget.refs.step.value);
-amount = Number(e.currentTarget.refs.amount.value);
-
-  if (delay >= 0 && step >= 0 && amount > 0) {
-    for (let position = 1; position <= amount; position += 1) {
-      delay += step;
-      createPromise(position, delay)
-        .then(({ position, delay }) => {
-          setTimeout(() => {
-            Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
-              useIcon: false,
-            });
-          }, delay);
-        })
-        .catch(({ position, delay }) => {
-          setTimeout(() => {
-            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-              useIcon: false,
-            });
-          }, delay);
-        });
-    }
-  } else {
-    Notify.warning('Put value > 0');
-  }
+  creatBtn: document.querySelector('.creatBtn')
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  const objectPromise = { position, delay };
-
-  return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve(objectPromise);
-    }
-    reject(objectPromise);
-  });
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+
+
+
+refs.creatBtn.addEventListener('click', (evt) => {
+
+  setTimeout(() => {
+    for (let i = 0; i < amount; i++) {
+      createPromise(position, delay)
+    }
+  }, refs.delay)
+
+})
